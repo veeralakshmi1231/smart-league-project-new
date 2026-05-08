0ebbc1a936d1df146648d13b7ec20cfd6044dcba
Esther Silviya <esthersilviya900@gmail.com>
2026-05-08 08:40:15 +0530

    Fix: Add timeouts to Nodemailer to prevent hanging

--- server.js
+++ server.js
@@ -102,7 +102,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  pool: true
+  connectionTimeout: 10000, // 10 seconds
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 console.log("Nodemailer: Starting connection verification...");
